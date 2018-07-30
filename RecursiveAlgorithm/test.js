19688862625ea26ddd365eb11236062b7fe77060
Kevin7c <cnd245006350@163.com>
2018-07-30 22:00:12 +0800

    基础递归

---
+++ RecursiveAlgorithm/test.js
@@ -0,0 +1,19 @@
+/**
+ * 递归
+ */
+(function () {
+    var fn=function (n) {
+        if(n<=1){
+            return 1;
+        }
+        return n*fn(n-1);
+    };
+    var num=fn(5);
+    console.log(num);
+    var div=document.createElement('div');
+    div.innerHTML=num;
+    var body=document.getElementsByTagName('body')[0];
+    console.log(body);
+    console.log(document.body);
+    body.appendChild(div);
+})();
